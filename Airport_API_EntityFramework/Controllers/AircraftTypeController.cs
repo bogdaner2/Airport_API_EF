f8b7d29b967e1333f9746b804febefd9b44be163
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-07-13 13:41:23 +0300

    Created initial project

---
+++ Airport_API_EntityFramework/Controllers/AircraftTypeController.cs
@@ -0,0 +1,61 @@
+﻿using Airport_REST_API.Services.Interfaces;
+using Airport_REST_API.Shared.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airport_API_EntityFramework.Controllers
+{
+    [Route("api/[controller]")]
+    public class AircraftTypeController : Controller
+    {
+        private readonly IAircraftTypeService _service;
+        public AircraftTypeController(IAircraftTypeService service)
+        {
+            _service = service;
+        }
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.GetData());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_service.GetObject(id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]AircraftTypeDTO type)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = _service.AddObject(type);
+            return result == true ? StatusCode(200) : StatusCode(404);
+        }
+
+        // PUT api/aircrafttype/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]AircraftTypeDTO type)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = _service.AddObject(type);
+            return result == true ? StatusCode(200) : StatusCode(404);
+        }
+
+        // DELETE api/aircrafttype/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _service.RemoveObject(id);
+            return result == true ? StatusCode(200) : StatusCode(404);
+        }
+    }
+}
---
+++ Airport_API_EntityFramework/Controllers/TicketController.cs
@@ -0,0 +1,61 @@
+﻿using Airport_REST_API.Services.Interfaces;
+using Airport_REST_API.Shared.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airport_API_EntityFramework.Controllers
+{
+    [Route("api/[controller]")]
+    public class TicketController : Controller
+    {
+        private readonly ITicketService _service;
+        public TicketController(ITicketService service)
+        {
+            _service = service;
+        }
+        // GET api/ticket
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_service.GetData());
+        }
+
+        // GET api/ticket/:id
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_service.GetObject(id));
+        }
+
+        // POSt api/ticket
+        [HttpPost]
+        public IActionResult Post([FromBody]TicketDTO ticket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = _service.AddObject(ticket);
+            return result == true ? StatusCode(200) : StatusCode(404);
+        }
+
+        // PUT api/ticket
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id,[FromBody]TicketDTO ticket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = _service.UpdateObject(id,ticket);
+            return result == true ? StatusCode(200) : StatusCode(404);
+        }
+
+        // PUT api/ticket
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _service.RemoveObject(id);
+            return result == true ? StatusCode(200) : StatusCode(404);
+        }
+    }
+}
